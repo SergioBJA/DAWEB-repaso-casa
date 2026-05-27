f25a5ce0e87a7cf478a8898651d77bede9946851
sergiobja <sergiobohorquezj@gmail.com>
2026-05-27 20:10:35 +0200

    Apartado 2: Añadido endpoint de status con mis datos

--- app/main.py
+++ app/main.py
@@ -11,5 +11,5 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Apellidos, Nombre - v.X.Y"}
+    return {"status": "Bohórquez Jesús, Sergio - v.1.0"}
 
